97383ba88c8a59096d6041c287c281bdeecab206
Vitalij Nykyforenko <vitalij.nykyforenko@gmail.com>
2019-10-14 11:37:36 +0300

    init

---
+++ android/app/src/main/kotlin/com/cjvnjde/todo/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.cjvnjde.todo
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
